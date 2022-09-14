cc5a0b1259f4e99555ce389f52681d58f1da9c5e
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-14 17:55:52 -0500

    Create methods for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :comments
   has_many :likes
+
+  def update_user_counter
+    author.update!(posts_counter: Post.count)
+  end
+
+  def recent_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
