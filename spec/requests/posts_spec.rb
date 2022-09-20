ab315a0c1fc76f96885bd6e43a4ac417100a569f
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-20 12:43:32 -0500

    Fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe 'Posts', type: :request do
     it 'response status is correct' do
       expect(response).to have_http_status(:ok)
     end
-    
+
     it 'correct template was rendered' do
-      expect(response).to render_template "posts/index"
+      expect(response).to render_template 'posts/index'
     end
 
     it 'correct template was rendered testing without path' do
@@ -20,12 +20,11 @@ RSpec.describe 'Posts', type: :request do
     end
 
     describe 'GET /show' do
-
       before(:each) { get user_post_path(1, 3) }
       it 'response status is correct' do
         expect(response).to have_http_status(200)
       end
-    
+
       it 'correct template show was rendered' do
         expect(response).to render_template(:show)
       end
