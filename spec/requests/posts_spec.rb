90d7ede01dc8030a80b4ef733d82a428eaeb3f87
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-20 12:38:01 -0500

    Add posts required tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) { get user_posts_path(1) }
+    it 'response status is correct' do
+      expect(response).to have_http_status(:ok)
+    end
+    
+    it 'correct template was rendered' do
+      expect(response).to render_template "posts/index"
+    end
+
+    it 'correct template was rendered testing without path' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder text.' do
+      expect(response.body).to include('posts index here')
+    end
+
+    describe 'GET /show' do
+
+      before(:each) { get user_post_path(1, 3) }
+      it 'response status is correct' do
+        expect(response).to have_http_status(200)
+      end
+    
+      it 'correct template show was rendered' do
+        expect(response).to render_template(:show)
+      end
+
+      it 'response body includes correct placeholder text.' do
+        expect(response.body).to include('posts show here')
+      end
+    end
   end
 end
