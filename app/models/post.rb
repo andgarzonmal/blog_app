75d0a2b1dedb1370cd813c7ab4e539a2ae57d54a
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-21 12:47:55 -0500

    refactor posts counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,13 +8,13 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_initialize do |post|
-    post.comments_counter = 0
-    post.likes_counter = 0
-  end
+  # after_initialize do |post|
+  #   post.comments_counter = 0
+  #   post.likes_counter = 0
+  # end
 
   def update_user_counter
-    author.increment!(:posts_counter)
+    author.update!(posts_counter: author.posts.count)
   end
 
   def recent_5_comments
