3d48555f44e95fdda9ada66cc2094e86672372d4
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-21 12:48:12 -0500

    Remove extra code

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_initialize do |user|
-    user.posts_counter = 0
-  end
+  # after_initialize do |user|
+  #   user.posts_counter = 0
+  # end
 
   def recent_3_posts
     posts.order(created_at: :desc).limit(3)
