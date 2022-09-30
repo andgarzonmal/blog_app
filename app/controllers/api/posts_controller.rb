7147e35c86fa1b69af57ed52a86d948431811307
Gonzalo Medina <41912912+mgmediaweb@users.noreply.github.com>
2022-09-30 07:43:55 -0700

    Update posts_controller.rb

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::PostsController < ApplicationController
-  # before_action :authenticate_user!
-  # respond_to :json
+  before_action :authenticate_user!
+  respond_to :json
   # def index
   #   @posts = current_user.posts
   #   render json: { posts: @posts }, status: :ok
