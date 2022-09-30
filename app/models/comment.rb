b0d61ba8c9fc5b27830c378a309a1fa723a0228f
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-30 09:34:54 -0500

    Add validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :update_comments_counter
+  validates :text, presence: true
 
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
