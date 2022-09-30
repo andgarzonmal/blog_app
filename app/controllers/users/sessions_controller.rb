ab00e8a7c4a1fd3a533b694c8df075aec387d6ae
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-29 22:12:28 -0500

    Add login feature

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,7 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json, :html
   # before_action :configure_sign_in_params, only: [:create]
+  skip_before_action :verify_authenticity_token
 
   # GET /resource/sign_in
   # def new
@@ -10,9 +11,16 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with(resource) do |format|
+      format.html {redirect_to after_sign_in_path_for(resource)}
+      format.json {render json: {message: 'succesfully logged in'}, status: :ok }
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
